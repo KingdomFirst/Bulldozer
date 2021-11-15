97fdef2ea19fcb504ec8bdb19c14f43b5d3dfdcd
guy <guym@kingdomfirstsolutions.com>
2021-11-15 17:16:30 -0500

    Add alternate email support

--- Bulldozer.CSV/CSVComponent.cs
+++ Bulldozer.CSV/CSVComponent.cs
@@ -669,6 +669,7 @@ namespace Bulldozer.CSV
         private const int MedicalNote = 28;
         private const int SecurityNote = 29;
         private const int PreviousPersonIds = 30;
+        private const int AlternateEmails = 31;
 
         #endregion Individual Constants
 
