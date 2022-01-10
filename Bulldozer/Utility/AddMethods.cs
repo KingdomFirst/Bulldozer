624734c70041507a09d90b1a80882d26b82b3fc5
guy <guym@kingdomfirstsolutions.com>
2022-01-10 14:45:00 -0500

    Update copyright

--- Bulldozer/Utility/AddMethods.cs
+++ Bulldozer/Utility/AddMethods.cs
@@ -1,5 +1,5 @@
 // <copyright>
-// Copyright 2019 by Kingdom First Solutions
+// Copyright 2022 by Kingdom First Solutions
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
